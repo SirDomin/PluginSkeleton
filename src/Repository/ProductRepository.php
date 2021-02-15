ff0db9fe865cda607b98f1fc9ab9b51266621263
SirDomin <dominik.garbulski@sylius.com>
2021-02-15 14:36:33 +0100

    xd

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -5,7 +5,21 @@ declare(strict_types=1);
 
 namespace Acme\SyliusExamplePlugin\Repository;
 
-final class ProductRepository
-{
+use Sylius\Bundle\CoreBundle\Doctrine\ORM\ProductRepository as BaseProductRepository;
 
+class ProductRepository extends BaseProductRepository implements ProductRepositoryInterface
+{
+    public function findAllByOnHand(int $limit = 8): array
+    {
+        return $this->createQueryBuilder('o')
+            ->addSelect('variant')
+            ->addSelect('translation')
+            ->leftJoin('o.variants', 'variant')
+            ->leftJoin('o.translations', 'translation')
+            ->addOrderBy('variant.onHand', 'ASC')
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult()
+            ;
+    }
 }
--- src/Repository/ProductRepositoryInterface.php
+++ src/Repository/ProductRepositoryInterface.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Acme\SyliusExamplePlugin\Repository;
 
-interface ProductRepositoryInterface
+use Sylius\Component\Core\Repository\ProductRepositoryInterface as BaseProductRepositoryInterface;
+
+interface ProductRepositoryInterface extends BaseProductRepositoryInterface
 {
+    public function findAllByOnHand(int $limit): array;
 
 }
